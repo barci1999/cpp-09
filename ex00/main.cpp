c5d9bae10f298561e1427028dd3161d834e75b91
Pablo Alvarez Barciela <pablalva@c1r9s3.42madrid.com>
2025-11-03 15:53:41 +0100

    borrados algunos comentarios

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pablalva <pablalva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 13:06:00 by pablalva          #+#    #+#             */
-/*   Updated: 2025/11/03 15:19:12 by pablalva         ###   ########.fr       */
+/*   Updated: 2025/11/03 15:51:59 by pablalva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ int main(int argc,char **argv)
 {
 	if(argc != 2)
 	{
-		return(1);
 		std::cout<<"Error: could not open file."<<std::endl;
+		return(1);
 	}
 	try
 	{
@@ -36,7 +36,6 @@ int main(int argc,char **argv)
 		b.multiply(fd_file);
 		std::string input;
 		std::string date;
-		//float value;
 		while (std::getline(fd_file,input))
 		{
 			b.multiply(fd_file);
